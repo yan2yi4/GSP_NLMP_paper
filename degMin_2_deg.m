4e1be41c915e0d05558cf6dc3ec7b7a7ce74a8df
yan2yi4 <yiyiyiyan@gmail.com>
2022-05-31 01:11:00 +0800

    Delete degMin_2_deg.m

--- degMin_2_deg.m
+++
@@ -1,6 +0,0 @@
-function [degreesFloat] = degMin_2_deg(degreesInt,minInt)
-%UNTITLED Summary of this function goes here
-%   Detailed explanation goes here
-    degreesFloat = degreesInt + minInt/60;
-end
-
